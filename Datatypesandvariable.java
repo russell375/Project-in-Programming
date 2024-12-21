39cb643ede56a3ff8809e4040209eb83ab637397
russell375 <razelmartinez506@gmail.com>
2024-12-21 17:16:30 +0800

    Add files via upload

---
+++ Datatypesandvariable.java
@@ -0,0 +1,19 @@
+public class Datatypesandvariable {
+    public static void main(String[] args) {
+        // Declare and initialize a variable of type String
+        String name = "Janecres";
+        short num = 14389;
+        byte myNum = 123;
+        int numbers = 12345;
+        long phoneNumber = 9512037119L;
+        float fl = 3.14f;
+        char ch = 'A';
+        boolean isStudent = true;
+
+
+    }
+}
+
+      
+   
+
