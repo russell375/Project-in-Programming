db58035f5c94d99126aed35b2186aa2022596dd1
russell375 <razelmartinez506@gmail.com>
2024-12-21 16:53:24 +0800

    Update BasicSyntax.java

---
+++ BasicSyntax.java
@@ -0,0 +1,13 @@
+public class Basicsyntax {
+public static void main(String[] args) {
+    String name = "Kim";
+    int age = 25;
+    double allowance = 100.25;
+    boolean isStudent = true;
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Allowance: " + allowance);
+
+}
+    
+}
