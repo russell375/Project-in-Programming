9c6b7c4790fdfc2bce85c1ad4236609db6e68de6
russell375 <razelmartinez506@gmail.com>
2024-12-22 10:36:02 +0800

    Add files via upload

---
+++ Data Structure.java
@@ -0,0 +1,39 @@
+public class Datastructure {
+ public static void main(String[] args) {
+
+  // Array(String)
+  String[] classmates = new String[6];
+  classmates[0] = "Kim";
+  classmates[1] = "Razel";
+  classmates[2] = "Janecres";
+  classmates[3] = "Dahulo";
+  classmates[4] = "Goryo";
+  classmates[5] = "Cardo";
+
+ //Array(Int)
+ int[] age = new int[6];
+ age[0] = 18;
+ age[1] = 19;
+ age[2] = 23;
+ age[3] = 35;
+ age[4] = 41;
+ age[5] = 46;
+
+ //Array Boolean
+ boolean[] student = new boolean[6];
+ student[0] = true;
+ student[1] = true;
+ student[2] = true; 
+ student[3] = false;
+ student[4] = false;
+ student[5] = false;
+
+ //Print out String,Int, and Boolean
+ System.out.println(classmates[3]);
+ System.out.println(age[3]);
+ System.out.println(student[0]);
+
+}
+} 
+
+
