d246cfc42af8c390478436a6d1deb17024eda905
russell375 <razelmartinez506@gmail.com>
2024-12-25 20:23:35 +0800

    Add files via upload

---
+++ Classes1(Classes).java
@@ -0,0 +1,16 @@
+public class Classes1 {
+    public static void main(String[] args) {
+        //Class for names
+        Classes ourNames = new Classes();
+
+        //Class for age
+        Classes ourAge = new Classes();
+
+        //Output for names
+        System.out.println(ourNames.name3);
+
+        //Output for age
+        System.out.println(ourAge.age1);
+    
+}
+}
