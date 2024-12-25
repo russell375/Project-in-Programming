d2996340d4ce35dc60ea493d11b23e82be84e859
russell375 <razelmartinez506@gmail.com>
2024-12-25 17:26:33 +0800

    Add files via upload

---
+++ Main(OOP).java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    
+    Flashlight f = new Flashlight();
+
+    System.out.println(f.type);
+    System.out.println(f.size);
+    System.out.println(f.color);
+    System.out.println(f.batchNum);
+    System.out.println(f.clicked);
+
+
+}
+}
