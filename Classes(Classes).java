c2d8e7ce93a9b0faab8bf657178ba4f771fa5f04
russell375 <razelmartinez506@gmail.com>
2024-12-25 20:22:15 +0800

    Add files via upload

---
+++ Classes(Classes).java
@@ -0,0 +1,11 @@
+public class Classes {
+    //Input for name class
+   String name1 = "Kim Endro";
+   String name2 = "Razel";
+   String name3 = "Janecres";
+
+   //Input for age class
+   int age1 = 18;
+   int age2 = 19;
+   int age3 = 25;
+}
